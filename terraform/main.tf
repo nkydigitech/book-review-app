007690d682574a412079a1a73c7de50c49bad88e
Nkechi Ahanonye <ahanonyeanna@gmail.com>
2026-06-27 16:09:06 +0100

    Fix: use kubernetes_version (not cluster_version) in EKS module call

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,12 @@
 module "vpc" {
   source = "./modules/vpc"
 
-  project_name        = var.project_name
-  environment         = var.environment
-  vpc_cidr            = var.vpc_cidr
-  public_subnet_cidrs = var.public_subnet_cidrs
+  project_name         = var.project_name
+  environment          = var.environment
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
-  availability_zones  = var.availability_zones
+  availability_zones   = var.availability_zones
 }
 
 module "eks" {
@@ -20,7 +20,7 @@ module "eks" {
   vpc_id             = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnet_ids
   public_subnet_ids  = module.vpc.public_subnet_ids
-  cluster_version    = var.cluster_version
+  kubernetes_version = var.cluster_version
   node_instance_type = var.node_instance_type
   node_desired_size  = var.node_desired_size
   node_min_size      = var.node_min_size
